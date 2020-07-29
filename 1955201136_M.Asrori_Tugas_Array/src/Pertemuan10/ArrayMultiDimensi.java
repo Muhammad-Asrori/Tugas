40dac72d652dce5edb85f5b1fdb9fd3bda0cd7ad
Muhammad-Asrori <66908539+Muhammad-Asrori@users.noreply.github.com>
2020-07-29 15:12:10 +0700

    Add files via upload

---
+++ 1955201136_M.Asrori_Tugas_Array/src/Pertemuan10/ArrayMultiDimensi.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Pertemuan10;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class ArrayMultiDimensi {
+    public static void main(String[] args) {
+        
+        int[][] nilai = { 
+            {33,19},
+            {05,27}
+        }; 
+
+        
+        System.out.println(nilai[1][0]);
+        
+    }
+    
+}
---
+++ 1955201136_M.Asrori_Tugas_Array/src/Pertemuan10/ArraySatuDimensi.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Pertemuan10;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class ArraySatuDimensi {
+
+    /**
+     * @param args the command line arguments 
+     */
+    public static void main(String[] args) {
+        
+       int angka[] = {5,12,8,27};
+        
+       System.out.println("Pilihlah Angka Berikut ini !");
+       
+       for(int i=0; i<angka.length; i++)
+            System.out.println(angka[i]);
+       
+            System.out.println("Angka Pilihanmu Adalah : "+ angka[3]);
+        
+        
+        
+        
+      
+            
+        
+        
+        
+        
+    }
+    
+}
